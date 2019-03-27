64a5d46c0e89bfdd429addafa05874f5f7baff1b
pavvo <vv.pavlee@gmail.com>
2019-03-27 10:51:56 +0100

    First commit

---
+++ frontend/components/hocs/defaultPage.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Router from "next/router";
+
+import { getUserFromServerCookie, getUserFromLocalCookie } from "../lib/auth";
--- frontend/pages/restaurants.js
+++ frontend/pages/restaurants.js
@@ -19,10 +19,10 @@ import {
 const GET_RESTAURANT_DISHES = gql`
   query($id: ID!) {
     restaurant(id: $id) {
-      _id
+      id
       name
       dishes {
-        _id
+        id
         name
         description
         price
@@ -34,18 +34,92 @@ const GET_RESTAURANT_DISHES = gql`
   }
 `;
 
+class Restaurants extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    const {
+      data: { loading, error, restaurant },
+      router,
+      context,
+      isAuthenticated
+    } = this.props;
+
+    if (error) {
+        console.log(error.message)
+    };
+
+    if (restaurant) {
+      return (
+        <>
+          <h1>{restaurant.name}</h1>
+          <Row>
+            <Col xs="9" style={{ padding: 0 }}>
+              <div style={{ display: "inline-block" }} className="h-100">
+                {restaurant.dishes.map(res => (
+                  <Card
+                    style={{ width: "30%", margin: "0 10px" }}
+                    key={res.id}
+                  >
+                    <CardImg
+                      top={true}
+                      style={{ height: 250 }}
+                      src={`http://localhost:1337${res.image.url}`}
+                    />
+                    <CardBody>
+                      <CardTitle>{res.name}</CardTitle>
+                      <CardText>{res.description}</CardText>
+                    </CardBody>
+                    <div className="card-footer">
+                      <Button outline color="primary">
+                        + Add To Cart
+                      </Button>
+
+                      <style jsx>
+                        {`
+                          a {
+                            color: white;
+                          }
+                          a:link {
+                            text-decoration: none;
+                            color: white;
+                          }
+                          .container-fluid {
+                            margin-bottom: 30px;
+                          }
+                          .btn-outline-primary {
+                            color: #007bff !important;
+                          }
+                          a:hover {
+                            color: white !important;
+                          }
+                        `}
+                      </style>
+                    </div>
+                  </Card>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </>
+      );
+    }
+    return <h1>Loading</h1>;
+  }
+}
 
 export default compose(
-    withRouter,
-    graphql(GET_RESTAURANT_DISHES, {
-      options: props => {
-        return {
-          variables: {
-            id: props.router.query.id
-          }
-        };
-      },
-      props: ({ data }) => ({ data })
-    })
-  )(Restaurants);
+  withRouter,
+  graphql(GET_RESTAURANT_DISHES, {
+    options: props => {
+      return {
+        variables: {
+          id: props.router.query.id
+        }
+      };
+    },
+    props: ({ data }) => ({ data })
+  })
+)(Restaurants);
